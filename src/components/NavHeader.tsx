f1e00e5e0dec87d7dd8d6494c1353706708e9b43
sumionochi <aaditya.srivastava.connect@gmail.com>
2023-12-09 18:39:51 +0530

    complete Navbar state change b/w login and logout

--- src/components/NavHeader.tsx
+++ src/components/NavHeader.tsx
@@ -5,21 +5,25 @@ import { MessageSquareIcon } from 'lucide-react'
 import { Button } from './ui/button'
 import { Themetoggle } from './ui/Themetoggle'
 import Logo from './Logo'
+import { UserButton, auth } from '@clerk/nextjs'
 
 type Props = {}
 
 const NavHeader = async (props: Props) => {
+  const {userId} = auth();
+  console.log()
   return (
     <header className='sticky top-0 z-50 backdrop-blur-sm mx-auto'>
       <nav className='flex max-w-7xl gap-2 flex-col sm:flex-row items-center p-5 pl-2 bg-none mx-auto'>
         <Logo/>
         <div className='flex-1 flex items-center justify-end space-x-4'>
           <div className='flex gap-4 bg-secondary p-2 rounded-lg'>
-            <Link href={'/chat'} prefetch={false}>
-              <Button variant={'ghost'} className=''>
-                <MessageSquareIcon className='text-primary w-5 h-5'/>
-              </Button>
-            </Link>
+            {userId &&
+              <div className='flex flex-row gap-4'> 
+                <UserButton afterSignOutUrl='/' appearance={{elements:{avatarBox:{width:'2.5rem', height:"2.5rem"}}}}/>
+                <Button>Add Event</Button>
+              </div>
+            }
             <Themetoggle/>
           </div>
         </div>
---
+++ src/components/ui/Typewriter.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { Bot, Rocket } from "lucide-react";
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+type Props = {};
+
+const TypewriterTitle = (props: Props) => {
+  return (
+    <Typewriter 
+      options={{
+        loop: true,
+      }}
+      onInit={(typewriter) => {
+        typewriter
+          .typeString(`Health,`)
+          .pauseFor(1000)
+          .deleteAll()
+          .typeString(`Panic Attacks,`)
+          .pauseFor(1000)
+          .deleteAll()
+          .typeString(`Migraines,`)
+          .pauseFor(1000)
+          .deleteAll()
+          .typeString(`Seizures,`)
+          .pauseFor(1000)
+          .deleteAll()
+          .typeString(`Blackouts,`)
+          .start();
+      }}
+    />
+  );
+};
+
+export default TypewriterTitle;
